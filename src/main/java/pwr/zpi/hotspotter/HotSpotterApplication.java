30ce166310fc52593071ed7547ed88778306c8a8
Michał Sosnowski <sosnowskimichal.028@gmail.com>
2025-10-21 16:54:45 +0200

    Initial commit

---
+++ src/main/java/pwr/zpi/hotspotter/HotSpotterApplication.java
@@ -0,0 +1,13 @@
+package pwr.zpi.hotspotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotSpotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwr/zpi/hotspotter/HotSpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.zpi.hotspotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSpotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
