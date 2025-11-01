518bbeee5b73d2a02c27d4bb7254edc420af6e5e
Michał Wąsiński <micwasi2003@gmail.com>
2025-11-01 16:28:42 +0100

    feat: remove unused import of SonarAnalysisState in SonarAnalysisStatusRepository

--- src/main/java/pwr/zpi/hotspotter/sonar/repository/SonarAnalysisStatusRepository.java
+++ src/main/java/pwr/zpi/hotspotter/sonar/repository/SonarAnalysisStatusRepository.java
@@ -2,7 +2,6 @@ package pwr.zpi.hotspotter.sonar.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import pwr.zpi.hotspotter.sonar.model.analysisstatus.SonarAnalysisState;
 import pwr.zpi.hotspotter.sonar.model.analysisstatus.SonarAnalysisStatus;
 
 import java.util.Optional;
