aa86306e1739a03e00ce6416c54e5f82d25dcea6
Michał Wąsiński <micwasi2003@gmail.com>
2025-11-02 18:56:48 +0100

    feat: add authentication module with JWT and OAuth2 support

---
+++ src/main/java/pwr/zpi/hotspotter/authentication/service/JwtService.java
@@ -0,0 +1,75 @@
+package pwr.zpi.hotspotter.authentication.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import pwr.zpi.hotspotter.authentication.config.JwtProperties;
+import pwr.zpi.hotspotter.user.model.User;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user) {
+        Date now = new Date();
+        Date exp = new Date(System.currentTimeMillis() + jwtProperties.getExpiration());
+
+        return Jwts.builder()
+                .claim("userId", String.valueOf(user.getId()))
+                .claim("email", user.getEmail())
+                .claim("role", String.valueOf(user.getRole()))
+                .subject(user.getEmail())
+                .issuedAt(now)
+                .expiration(exp)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractUserId(String token) {
+        return extractClaim(token, claims -> claims.get("userId", String.class));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecret());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
