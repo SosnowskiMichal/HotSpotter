27cd65b08301c2842d34da05c4712821a3a246ea
Michał Sosnowski <sosnowskimichal.028@gmail.com>
2025-11-03 20:38:57 +0100

    Changed allowed repository URLs

--- src/main/java/pwr/zpi/hotspotter/repositorymanagement/service/parser/RepositoryUrlParser.java
+++ src/main/java/pwr/zpi/hotspotter/repositorymanagement/service/parser/RepositoryUrlParser.java
@@ -3,7 +3,6 @@ package pwr.zpi.hotspotter.repositorymanagement.service.parser;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,7 +10,7 @@ import java.util.regex.Pattern;
 public class RepositoryUrlParser {
 
     private static final Pattern REPOSITORY_URL_PATTERN = Pattern.compile(
-            "^(?:https://(?<platformHttps>git(?:hub|lab))\\.com/|(?<platformSsh>git@git(?:hub|lab))\\.com:)"
+            "^(?:https://)?(?<platform>git(?:hub|lab))\\.com/"
             + "(?<owner>[^/]+)/(?<name>(?!\\.git$)[^/]+?)(?:\\.git)*$"
     );
 
@@ -19,13 +18,25 @@ public class RepositoryUrlParser {
         Matcher matcher = REPOSITORY_URL_PATTERN.matcher(repositoryUrl);
         if (!matcher.matches()) {
             throw new IllegalArgumentException("Invalid repository URL: " + repositoryUrl
-                    + ", only GitHub and GitLab URLs are supported");
+                    + ", only GitHub and GitLab HTTPS URLs are supported");
         }
 
-        String platform = Objects.requireNonNullElse(matcher.group("platformHttps"), matcher.group("platformSsh"));
+        String platform = matcher.group("platform");
         String owner = matcher.group("owner");
         String name = matcher.group("name");
+        repositoryUrl = normalizeRepositoryUrl(repositoryUrl);
+
         return new RepositoryData(repositoryUrl, platform, owner, name);
+    }
+
+    private String normalizeRepositoryUrl(String repositoryUrl) {
+        if (!repositoryUrl.startsWith("http")) {
+            repositoryUrl = "https://" + repositoryUrl;
+        }
+        if (repositoryUrl.endsWith(".git")) {
+            repositoryUrl = repositoryUrl.substring(0, repositoryUrl.length() - 4);
+        }
+        return repositoryUrl;
     }
 
     public record RepositoryData(String repositoryUrl, String platform, String owner, String name) {
