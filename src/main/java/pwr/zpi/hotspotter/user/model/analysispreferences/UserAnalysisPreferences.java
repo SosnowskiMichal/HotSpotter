e1ada4ef5a73e6cc62f2d940a7e2bc29d4d86e07
Michał Wąsiński <micwasi2003@gmail.com>
2025-11-10 14:43:41 +0100

    refactor: move FileCouplingSettings into UserAnalysisPreferences

--- src/main/java/pwr/zpi/hotspotter/user/model/analysispreferences/UserAnalysisPreferences.java
+++ src/main/java/pwr/zpi/hotspotter/user/model/analysispreferences/UserAnalysisPreferences.java
@@ -2,12 +2,25 @@ package pwr.zpi.hotspotter.user.model.analysispreferences;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class UserAnalysisPreferences {
-    private static final int DEFAULT_ANALYSIS_PERIOD_MONTHS = 6;
-    private static final int DEFAULT_ACTIVE_AUTHOR_MAX_INACTIVITY_MONTHS = 6;
+    private static final int DEFAULT_HOT_SPOT_ANALYSIS_PERIOD_MONTHS = 6;
+    private static final int DEFAULT_AUTHOR_INACTIVITY_THRESHOLD_MONTHS = 6;
 
-    private int analysisPeriodMonths = DEFAULT_ANALYSIS_PERIOD_MONTHS;
-    private int activeAuthorMaxInactivityMonths = DEFAULT_ACTIVE_AUTHOR_MAX_INACTIVITY_MONTHS;
-    private FileCouplingSettings fileCouplingSettings = new FileCouplingSettings();
+    private static final int DEFAULT_MIN_FILE_COMMITS = 5;
+    private static final int DEFAULT_MIN_SHARED_COMMITS = 5;
+    private static final double DEFAULT_MIN_FILE_COUPLING = 0.1;
+    private static final int DEFAULT_MAX_FILES_PER_COMMIT = 30;
+
+    private int hotSpotAnalysisPeriodMonths = DEFAULT_HOT_SPOT_ANALYSIS_PERIOD_MONTHS;
+    private int authorInactivityThresholdMonths = DEFAULT_AUTHOR_INACTIVITY_THRESHOLD_MONTHS;
+
+    private int minFileCommits = DEFAULT_MIN_FILE_COMMITS;
+    private int minSharedCommits = DEFAULT_MIN_SHARED_COMMITS;
+    private double minFileCoupling = DEFAULT_MIN_FILE_COUPLING;
+    private int maxFilesPerCommit = DEFAULT_MAX_FILES_PER_COMMIT;
+    private List<FileIgnorePattern> fileIgnorePatterns = new ArrayList<>();
 }
