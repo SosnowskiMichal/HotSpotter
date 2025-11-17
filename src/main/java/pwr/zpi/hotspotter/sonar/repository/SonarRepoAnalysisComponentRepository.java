a52a19869ccb9588022c3be0b514efb7b125127b
Michał Wąsiński <micwasi2003@gmail.com>
2025-11-17 16:57:56 +0100

    feat: Add Byte Buddy agent for testing and update repository annotations

--- src/main/java/pwr/zpi/hotspotter/sonar/repository/SonarRepoAnalysisComponentRepository.java
+++ src/main/java/pwr/zpi/hotspotter/sonar/repository/SonarRepoAnalysisComponentRepository.java
@@ -1,7 +1,6 @@
 package pwr.zpi.hotspotter.sonar.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 import pwr.zpi.hotspotter.sonar.model.repoanalysis.SonarRepoAnalysisComponent;
 
 import java.util.Optional;
