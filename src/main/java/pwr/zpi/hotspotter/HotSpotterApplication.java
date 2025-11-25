42e9ca6919eccb484cb6e522cb66197ea298cc19
Michał Sosnowski <sosnowskimichal.028@gmail.com>
2025-11-25 14:45:50 +0100

    Fixed error with calculating days between dates

--- src/main/java/pwr/zpi/hotspotter/HotSpotterApplication.java
+++ src/main/java/pwr/zpi/hotspotter/HotSpotterApplication.java
@@ -1,12 +1,23 @@
 package pwr.zpi.hotspotter;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 public class HotSpotterApplication {
 
-	public static void main(String[] args) {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        log.info("Application timezone set to: {}", TimeZone.getDefault().getID());
+    }
+
+	static void main(String[] args) {
 		SpringApplication.run(HotSpotterApplication.class, args);
 	}
 
